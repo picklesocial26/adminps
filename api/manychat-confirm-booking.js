535be1f7300dc3b482b5f25ea17501d1271a976e
picklesocial26 <picklesocial26@gmail.com>
2026-06-08 02:00:01 +0800

    Delete api/manychat-confirm-booking.js

--- api/manychat-confirm-booking.js
+++
@@ -1,9 +0,0 @@
-// ManyChat booking confirmation endpoint alias
-// This route uses the same booking confirmation logic as /api/confirm-booking
-// and is provided for ManyChat automation flows.
-
-const handler = require('./confirm-booking');
-
-module.exports = handler;
-module.exports.default = handler;
-module.exports.handler = handler;
